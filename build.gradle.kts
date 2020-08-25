702fe7f8361a6ea35c9140dd997c826d84d47b35
Adrian Cole <adrian.f.cole@gmail.com>
2020-08-25 17:01:16 +0800

    Refactors Pinot image to expand its jars
    While this increases the image size by 170m, it appears to get time
    to healthy reliably down to <20s (usually 16 on my laptop) without
    any changes to the Pinot codebase.
    
    Note: looking at verbose:gc, we definitely need 256m heap for Pinot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.3.0"
-  id("org.hypertrace.docker-publish-plugin") version "0.3.0"
+  id("org.hypertrace.docker-plugin") version "0.6.1"
+  id("org.hypertrace.docker-publish-plugin") version "0.6.1"
 }
 
 hypertraceDocker {
