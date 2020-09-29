6daaa1600a62ffc033501993762424a268b26355
Ronak <ronak@traceable.ai>
2020-09-29 17:08:46 +0530

    implement StreamMessageDecoder interface

--- pinot-servicemanager/build.gradle.kts
+++ pinot-servicemanager/build.gradle.kts
@@ -12,6 +12,7 @@ hypertraceDocker {
 
 dependencies {
   implementation("org.apache.pinot:pinot-spi:0.5.0")
+  implementation("org.apache.pinot:pinot-avro-base:0.5.0")
   implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-serdes:0.1.10")
 }
 
--- pinot-servicemanager/src/main/java/org/hypertrace/pinot/plugins/GenericAvroMessageDecoder.java
+++ pinot-servicemanager/src/main/java/org/hypertrace/pinot/plugins/GenericAvroMessageDecoder.java
@@ -1,12 +1,12 @@
 package org.hypertrace.pinot.plugins;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.concurrent.NotThreadSafe;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericData.Record;
-import org.apache.avro.io.BinaryDecoder;
-import org.apache.avro.io.DatumReader;
+import org.apache.pinot.plugin.inputformat.avro.AvroRecordExtractor;
 import org.apache.pinot.spi.data.readers.GenericRow;
 import org.apache.pinot.spi.data.readers.RecordExtractor;
 import org.apache.pinot.spi.plugin.PluginManager;
@@ -21,17 +21,17 @@ public class GenericAvroMessageDecoder implements StreamMessageDecoder<byte[]> {
   private static final Logger LOGGER = LoggerFactory.getLogger(GenericAvroMessageDecoder.class);
   GenericAvroSerde genericAvroSerde;
   String topicName;
-  private DatumReader<Record> _datumReader;
   private RecordExtractor<Record> _avroRecordExtractor;
-  private BinaryDecoder _binaryDecoderToReuse;
-  private GenericData.Record _avroRecordToReuse;
+
 
   @Override
   public void init(Map<String, String> props, Set<String> fieldsToRead, String topicName)
       throws Exception {
     this.topicName = topicName;
-    //_datumReader = new GenericDatumReader<>(_avroSchema);
+
     genericAvroSerde = new GenericAvroSerde();
+    genericAvroSerde.configure(props, false);
+
     String recordExtractorClass = props.get(RECORD_EXTRACTOR_CONFIG_KEY);
     // Backward compatibility to support Avro by default
     if (recordExtractorClass == null) {
@@ -51,5 +51,15 @@ public class GenericAvroMessageDecoder implements StreamMessageDecoder<byte[]> {
     GenericData.Record record = (GenericData.Record) genericAvroSerde.deserializer()
         .deserialize(this.topicName, payload);
     return _avroRecordExtractor.extract(record, destination);
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>NOTE: the payload should contain message content only (without header).
+   */
+  @Override
+  public GenericRow decode(byte[] payload, int offset, int length, GenericRow destination) {
+    return decode(Arrays.copyOfRange(payload, offset, offset + length), destination);
   }
 }
