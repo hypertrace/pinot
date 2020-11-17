d79aa72644dbff44d16e7953b052d9bf710e937e
Tim Mwangi <44785807+tim-mwangi@users.noreply.github.com>
2020-11-17 06:52:03 -0800

    feat: add udf for conditional and stringequals (#55)
    * feat: add udf for conditional and stringequals
    
    * Add some unit tests for the UDFs
    
    * "null" is null is "null"

--- pinot-udf/build.gradle.kts
+++ pinot-udf/build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
   `java-library`
+  jacoco
+  id("org.hypertrace.jacoco-report-plugin") version "0.1.0"
 }
 
 dependencies {
-  implementation("org.hypertrace.core.attribute.service:attribute-projection-functions:0.4.1")
+  implementation("org.hypertrace.core.attribute.service:attribute-projection-functions:0.7.0")
   compileOnly("org.apache.pinot:pinot-common:0.5.0")
+
+  testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+}
+
+tasks.test {
+  useJUnitPlatform()
 }
---
+++ pinot-udf/src/test/java/org/apache/pinot/scalar/function/hypertrace/HypertraceScalarFunctionsTest.java
@@ -0,0 +1,39 @@
+package org.apache.pinot.scalar.function.hypertrace;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.hypertrace.core.attribute.service.projection.functions.Hash;
+import org.junit.jupiter.api.Test;
+
+public class HypertraceScalarFunctionsTest {
+  @Test
+  public void testHash() {
+    assertEquals(Hash.hash("foo"), HypertraceScalarFunctions.hash("foo"));
+    assertEquals("null", HypertraceScalarFunctions.hash("null"));
+    assertEquals("null", HypertraceScalarFunctions.hash(null));
+  }
+
+  @Test
+  public void testDefaultString() {
+    assertEquals("foo", HypertraceScalarFunctions.defaultString("foo", "bar"));
+    assertEquals("bar", HypertraceScalarFunctions.defaultString("", "bar"));
+    assertEquals("bar", HypertraceScalarFunctions.defaultString("null", "bar"));
+    assertEquals("null", HypertraceScalarFunctions.defaultString("", "null"));
+  }
+
+  @Test
+  public void testStringEquals() {
+    assertEquals("true", HypertraceScalarFunctions.stringEquals("foo", "foo"));
+    assertEquals("false", HypertraceScalarFunctions.stringEquals("foo", ""));
+  }
+
+  @Test
+  public void testConditional() {
+    assertEquals("foo", HypertraceScalarFunctions.conditional("true", "foo", "bar"));
+    assertEquals("foo", HypertraceScalarFunctions.conditional("TRuE", "foo", "bar"));
+    assertEquals("bar", HypertraceScalarFunctions.conditional("FALSE", "foo", "bar"));
+    assertEquals("null", HypertraceScalarFunctions.conditional("null", "foo", "bar"));
+    assertEquals("null", HypertraceScalarFunctions.conditional(null, "foo", "bar"));
+    assertEquals("null", HypertraceScalarFunctions.conditional("false", "foo", null));
+  }
+}
