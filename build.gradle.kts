ccf59fdd4ea9d8988a411891d9cb8d88e127f0fd
Jayesh Bapu Ahire <jayesh.ahire@traceable.ai>
2021-01-07 20:39:27 +0530

    ci: migrating to GitHub action (#59)
    * adds GHA
    
    * removes cci
    
    * addressed Aaron's comments
    
    * addressed aaron's comments on kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.2.3"
-  id("org.hypertrace.docker-plugin") version "0.7.1"
-  id("org.hypertrace.docker-publish-plugin") version "0.7.1"
+  id("org.hypertrace.docker-plugin") version "0.8.1"
+  id("org.hypertrace.docker-publish-plugin") version "0.8.1"
 }
 
 hypertraceDocker {
