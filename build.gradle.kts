58f7c8ea056d5bf35a89febf46ee53a96c3d3db1
Ravi Singal <ravi.singal@traceable.ai>
2021-04-10 11:38:47 +0530

    publish artifacts to hypertrace artifactory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("org.hypertrace.repository-plugin") version "0.2.3"
-  id("org.hypertrace.docker-plugin") version "0.8.1"
-  id("org.hypertrace.docker-publish-plugin") version "0.8.1"
+  id("org.hypertrace.repository-plugin") version "0.4.0"
+  id("org.hypertrace.docker-plugin") version "0.9.0"
+  id("org.hypertrace.docker-publish-plugin") version "0.9.0"
 }
 
 hypertraceDocker {
