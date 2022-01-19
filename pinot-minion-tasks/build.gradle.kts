5a721621322cee9e08720f55453da0deea64e5af
Sarthak Singhal <sarthaksinghal@Sarthaks-MacBook-Pro.local>
2022-01-19 17:24:36 +0530

    added unit tests

--- pinot-minion-tasks/build.gradle.kts
+++ pinot-minion-tasks/build.gradle.kts
@@ -8,11 +8,10 @@ dependencies {
     compileOnly("org.apache.pinot:pinot-core:0.7.1")
     implementation ("org.apache.pinot:pinot-minion:0.7.1")
     implementation ("org.apache.pinot:pinot-controller:0.7.1")
-    testImplementation("org.mockito:mockito-core:2.10.0")
-    testImplementation("junit:junit:4.13.1")
-    testImplementation ("org.apache.pinot:pinot-integration-tests:0.7.1:tests")
+    testImplementation("org.mockito:mockito-core:2.23.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 tasks.test {
-    useJUnit()
+    useJUnitPlatform()
 }
--- pinot-minion-tasks/src/test/java/org/apache/pinot/plugin/minion/tasks/CustomerBasedRetentionTaskGeneratorTest.java
+++ pinot-minion-tasks/src/test/java/org/apache/pinot/plugin/minion/tasks/CustomerBasedRetentionTaskGeneratorTest.java
@@ -1,5 +1,98 @@
 package org.apache.pinot.plugin.minion.tasks;
 
+import static org.apache.pinot.plugin.minion.tasks.CustomerBasedRetentionConstants.TASK_TYPE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Lists;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.helix.task.TaskState;
+import org.apache.pinot.common.minion.RealtimeToOfflineSegmentsTaskMetadata;
+import org.apache.pinot.controller.helix.core.minion.ClusterInfoAccessor;
+import org.apache.pinot.core.common.MinionConstants;
+import org.apache.pinot.core.minion.PinotTaskConfig;
+import org.apache.pinot.spi.config.table.TableConfig;
+import org.apache.pinot.spi.config.table.TableTaskConfig;
+import org.apache.pinot.spi.config.table.TableType;
+import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
+import org.junit.jupiter.api.Test;
+
 public class CustomerBasedRetentionTaskGeneratorTest {
 
+  private static final String RAW_TABLE_NAME = "testTable";
+  private static final String OFFLINE_TABLE_NAME = "testTable_OFFLINE";
+
+  @Test
+  public void testGenerateTaskConfigs() {
+    ClusterInfoAccessor mockClusterInfoProvide = mock(ClusterInfoAccessor.class);
+
+    CustomerBasedRetentionTaskGenerator customerBasedRetentionTaskGenerator = new CustomerBasedRetentionTaskGenerator();
+    customerBasedRetentionTaskGenerator.init(mockClusterInfoProvide);
+
+    // Skip task generation, if realtime table
+    TableConfig realtimeTableConfig = new TableConfigBuilder(
+        TableType.REALTIME).setTableName(RAW_TABLE_NAME).build();
+    List<PinotTaskConfig> pinotTaskConfigs = customerBasedRetentionTaskGenerator.generateTasks(
+        Lists.newArrayList(realtimeTableConfig));
+    assertTrue(pinotTaskConfigs.isEmpty());
+
+    // No tableTaskConfig, error
+    TableConfig offlineTableConfig = getOfflineTableConfig(new HashMap<>());
+    offlineTableConfig.setTaskConfig(null);
+    try{
+      customerBasedRetentionTaskGenerator.generateTasks(Lists.newArrayList(offlineTableConfig));
+      fail("Should have failed for null tableTaskConfig");
+    } catch (NullPointerException e){}
+
+    // No taskConfig for task, error
+    offlineTableConfig = getOfflineTableConfig(new HashMap<>());
+    try{
+      customerBasedRetentionTaskGenerator.generateTasks(Lists.newArrayList(offlineTableConfig));
+      fail("Should have failed for null tableTaskConfig");
+    } catch (NullPointerException e){}
+  }
+
+  @Test
+  public void testEmptyTableConfig() {
+    ClusterInfoAccessor mockClusterInfoProvide = mock(ClusterInfoAccessor.class);
+    CustomerBasedRetentionTaskGenerator customerBasedRetentionTaskGenerator = new CustomerBasedRetentionTaskGenerator();
+    customerBasedRetentionTaskGenerator.init(mockClusterInfoProvide);
+    assertEquals(List.of(), customerBasedRetentionTaskGenerator.generateTasks(List.of()));
+  }
+
+  @Test
+  public void testGenerateTasksSimultaneousConstraints() {
+    Map<String, Map<String, String>> taskConfigsMap = new HashMap<>();
+    taskConfigsMap.put(TASK_TYPE, new HashMap<>());
+    TableConfig offlineTableConfig = getOfflineTableConfig(taskConfigsMap);
+
+    ClusterInfoAccessor mockClusterInfoProvide = mock(ClusterInfoAccessor.class);
+    Map<String, TaskState> taskStatesMap = new HashMap<>();
+    String taskName = "Task_CustomerBasedRetentionTask_" + System.currentTimeMillis();
+    Map<String, String> taskConfigs = new HashMap<>();
+    taskConfigs.put(MinionConstants.TABLE_NAME_KEY, OFFLINE_TABLE_NAME);
+
+    when(mockClusterInfoProvide.getTaskStates(TASK_TYPE)).thenReturn(taskStatesMap);
+    when(mockClusterInfoProvide.getTaskConfigs(taskName))
+        .thenReturn(Lists.newArrayList(new PinotTaskConfig(TASK_TYPE, taskConfigs)));
+    when(mockClusterInfoProvide.getMinionRealtimeToOfflineSegmentsTaskMetadata(OFFLINE_TABLE_NAME))
+        .thenReturn(new RealtimeToOfflineSegmentsTaskMetadata(OFFLINE_TABLE_NAME, 100_000L));
+
+    CustomerBasedRetentionTaskGenerator customerBasedRetentionTaskGenerator = new CustomerBasedRetentionTaskGenerator();
+    customerBasedRetentionTaskGenerator.init(mockClusterInfoProvide);
+
+    // if same task and table, IN_PROGRESS, then don't generate again
+    taskStatesMap.put(taskName, TaskState.IN_PROGRESS);
+    List<PinotTaskConfig> pinotTaskConfigs = customerBasedRetentionTaskGenerator.generateTasks(Lists.newArrayList(offlineTableConfig));
+    assertTrue(pinotTaskConfigs.isEmpty());
+  }
+
+  private TableConfig getOfflineTableConfig(Map<String, Map<String, String>> taskConfigsMap) {
+    return new TableConfigBuilder(TableType.OFFLINE).setTableName(RAW_TABLE_NAME).setTaskConfig(new TableTaskConfig(taskConfigsMap)).build();
+  }
 }
