dbef208314a263a798feb7532b48f360703a3d90
Senan Jude <senanjude@Senans-MacBook-Pro.local>
2022-07-20 20:17:25 +0530

    updated the version of docker-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("org.hypertrace.repository-plugin") version "0.4.0"
-  id("org.hypertrace.docker-plugin") version "0.9.0"
+  id("org.hypertrace.docker-plugin") version "0.9.4"
   id("org.hypertrace.docker-publish-plugin") version "0.9.4"
 }
 
